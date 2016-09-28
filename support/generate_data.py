dd41e4b17894c09c88fbd17dc06ee2a023945dd5
Krejca <krejci.cze@gmail.com>
2016-09-28 20:10:11 +0200

    Generator dat

--- support/generate_data.py
+++ support/generate_data.py
@@ -1,65 +1,35 @@
+import random
+
 #generate testing data
 filename = '../data/delays_dataset.csv'
 readF = open(filename, 'r')
+count = [10, 100, 1000, 10000, 100000, 1000000]
 
-filename = '../data/10_dataset.csv'
-writeF = open(filename, 'w')
-i = 10
-x = 0
-while x < i :
+for i in count :
+	filename = '../data/delays_dataset.csv'
+	readF = open(filename, 'r')
+	
+	name =""
+	if i > 999 :
+		name = str(int(i/1000)) + "k"
+	else :
+		name = str(int(i))
+	if i > 999999 :
+		name = str(int(i/1000000)) + "mio"
+	
+	filename = "../data/" + str(name) + "_dataset.csv"
+	writeF = open(filename, 'w')
+
+	#always write first line
 	line = readF.readline()
 	writeF.write (str(line))
-	x +=1
-writeF.close()
-
-filename = '../data/100_dataset.csv'
-writeF = open(filename, 'w')
-i = 100
-x = 0
-while x < i :
-	line = readF.readline()
-	writeF.write (str(line))
-	x +=1
-writeF.close()
-
-filename = '../data/1k_dataset.csv'
-writeF = open(filename, 'w')
-i = 1000
-x = 0
-while x < i :
-	line = readF.readline()
-	writeF.write (str(line))
-	x +=1
-writeF.close()
-
-filename = '../data/10k_dataset.csv'
-writeF = open(filename, 'w')
-i = 10000
-x = 0
-while x < i :
-	line = readF.readline()
-	writeF.write (str(line))
-	x +=1
-writeF.close()
-
-filename = '../data/100k_dataset.csv'
-writeF = open(filename, 'w')
-i = 100000
-x = 0
-while x < i :
-	line = readF.readline()
-	writeF.write (str(line))
-	x +=1
-writeF.close()
-
-filename = '../data/1mio_dataset.csv'
-writeF = open(filename, 'w')
-i = 1000000
-x = 0
-while x < i :
-	line = readF.readline()
-	writeF.write (str(line))
-	x +=1
-writeF.close()
+	x = 1
+	while x < i :
+		line = readF.readline()
+		#random writes i lines to the writeF
+		if int(random.random() * 10) == 0 :
+			writeF.write (str(line))
+			x +=1
+	writeF.close()
 
 readF.close()
