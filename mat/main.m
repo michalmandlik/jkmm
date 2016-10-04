2ec9d9a1d9de7ffee70cf7df03de0367478f0d31
mma <michalmandlik@gmail.com>
2016-10-04 20:54:49 +0200

    improved parser and plot

--- mat/main.m
+++ mat/main.m
@@ -3,32 +3,62 @@ clear
 close all
 
 fname = '../output/161002_000126_nodeList.csv'
-% fname = '../output/aa'
-% numlines = str2num( perl('count.pl', fname) );
 
-% fid = fopen(fname);
-% x=fscanf(fid,'%d',[2,2]);
-% 
-% 
-% fclose(fid);
-
+% load data file
 fileID = fopen(fname);
 dates = textscan(fileID,'%s %*[^\n]');
 fclose(fileID);
 
-
+% scan for unique char
 jj = 1;
 for ii = 1 : size(dates{1})
-    if dates{1,1}{ii,1}(1,1) == '+'
-        weekday{jj} = dates{1,1}{ii,1};
+    if dates{1,1}{ii,1}(1,1) == '>'
+        struct2anal{jj} = dates{1,1}{ii,1};
         jj = jj + 1;
     end
 end
 clear dates
 
-delimiter = ',';
-C = strsplit(weekday{1,1},delimiter);
-for ii = 2:size(C,2)
-    data(ii) =  str2double(C{1,ii});
+out(1) = 1;
+
+font_size_label = 14;
+font_size = 14;
+for jj = 1 : size(struct2anal, 2)
+    delimiter = ',';
+    C = strsplit(struct2anal{1,jj},delimiter);
+    for ii = 2:size(C,2)
+        data2anal{jj}(ii) =  str2double(C{1,ii});
+    end
+    c_delay = data2anal{jj}(2:2:end);
+    delay = data2anal{jj}(3:2:end);
+
+    out = c_delay.*delay;
+%    for ii = 1 : size(delay,2)
+%        out = [out, repmat(c_delay(ii), 1, delay(ii))];
+%     end
+    stat(jj, 1) = sum(c_delay.*delay) / sum(delay);
+    stat(jj, 2) = mean(out');
+    stat(jj, 3) = mode(out');
+    stat(jj, 4) = median(out');
+    stat(jj, 5) = std(c_delay.*delay);
+    clear out
+    h_figure = figure;
+    h_axes = axes();
+    plot(c_delay, 10*log10(delay),'b.','linewidth',1,'markersize', 12);
+    hold on
+    xlabel('Delay [hours]','FontSize',font_size_label); 
+    ylabel('Quantity [dB]','FontSize',font_size_label); 
+    title(['Histogram of arr apt ',C{1,1}([2])],'FontSize',font_size_label)
+    set(h_axes, 'FontSize', font_size)
+    set(h_figure, 'position',[200 100 800 700]); % dolni roh [x y] horni roh [x y]
+    set(h_figure(:),'color',[1 1 1]);
+    saveas(h_figure,['pics/arr_apt',C{1,1}([2]),'.tiff'])
+    %saveas(h_figure,['pics/hours',C{1,1}(2),'.fig'])
+%     close
+
+    out(1) = 1;
 end
+
+
+
     
